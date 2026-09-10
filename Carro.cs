2c2b0612a1bdf5e5a8042f082ee4ab8cbb74651b
Birardo <j.verderi@edu.senai.br>
2026-09-10 15:22:42 -0300

    Atividade 3

---
+++ Carro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Carro
+    {
+        private int _velocidadeAtual;
+
+        public void acelerar()
+        {
+            _velocidadeAtual += 10;
+            Console.WriteLine("O carro está acelerando!!");
+        }
+        public void frear()
+        {
+            if (_velocidadeAtual > 0)
+            {
+                _velocidadeAtual -= 10;
+                Console.WriteLine("O carro está freiando!!");
+            }
+            else
+            {
+                Console.WriteLine("O carro não pode frear mais!!");
+            }
+        }
+        public int velocidadeAtual
+        {
+            get
+            {
+                return _velocidadeAtual;
+            }
+        }
+    }
+}
